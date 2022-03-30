bf765abd59c93062d382c42bb82eb5eb0df97e72
Selena Nguyen <missselenah@gmail.com>
2022-03-29 21:26:23 -0700

    Add JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+function change_name() {
+    document.getElementById("new-name").innerHTML = "Sunshine Day";
+}
+
+/************** Connections *****************/
+
+function approvebtn(element) {
+    element.parentElement.remove();
+}
+
+function rejectbtn(element) {
+    element.parentElement.remove();
+}
+
+
